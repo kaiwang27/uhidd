0cb928bfdd4781893be0f79fe12ba2d160152b48
kaiw27 <kaiw27@b9b00da8-30de-11de-ba44-d58fe8a9ce33>
2010-02-13 02:43:38 +0000

    Use a in-memory key map for "multimedia keys". Whenever a multimedia key was pressed, the program will assign a free key code to it and record this map in the in-memory key map. When the same key was pressed again, the key code recorded in the map will be used.
    There are currently 37 "free" keys, which means we can support at
    most 37 multimedia keys in single one keyboard.

--- trunk/uhidd/uhidd_kbd.c
+++ trunk/uhidd/uhidd_kbd.c
@@ -42,13 +42,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -119,10 +112,11 @@ struct kbd_dev {
 	pthread_t kbd_task;
 	pthread_t kbd_status_task;
 	pthread_mutex_t kbd_mtx;
+	void *kbd_context;
 	uint32_t now;
 	int delay1;
 	int delay2;
-	int (*kbd_tr)(int);	/* Keycode translate function. */
+	int (*kbd_tr)(void *, int);	/* Keycode translate function. */
 
 #define KB_DELAY1	500
 #define KB_DELAY2	100
@@ -426,8 +420,10 @@ do {				\
 } while (0)
 
 static int
-kbd_hid2key(int hid_key)
+kbd_hid2key(void *context, int hid_key)
 {
+
+	(void) context;		/* unused. */
 
 	if (hid_key >= xsize) {
 		printf("Invalid keycode(%d) received\n", hid_key);
@@ -445,7 +441,7 @@ kbd_write(struct kbd_dev *kd, int hid_key, int make)
 	assert(kd->kbd_tr != NULL);
 
 	/* Ignore unmapped keys. */
-	if ((c = kd->kbd_tr(hid_key)) == -1)
+	if ((c = kd->kbd_tr(kd->kbd_context, hid_key)) == -1)
 		return;
 
 	n = 0;
@@ -680,7 +676,18 @@ kbd_input(struct hid_appcol *ha, uint8_t mod, uint16_t *keycodes, int key_cnt)
 }
 
 void
-kbd_set_tr(struct hid_appcol *ha, int (*tr)(int))
+kbd_set_context(struct hid_appcol *ha, void *context)
+{
+	struct kbd_dev *kd;
+
+	kd = hid_appcol_get_private(ha);
+	assert(kd != NULL);
+
+	kd->kbd_context = context;
+}
+
+void
+kbd_set_tr(struct hid_appcol *ha, int (*tr)(void *, int))
 {
 	struct kbd_dev *kd;
 
